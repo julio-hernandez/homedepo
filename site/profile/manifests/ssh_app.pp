b567c1ebca5438db5043cd3aba55ddc42d37772d
julio-hernandez <tenek75@gmail.com>
2018-02-23 22:22:50 -0500

    Update ssh_app.pp

--- site/profile/manifests/ssh_app.pp
+++ site/profile/manifests/ssh_app.pp
@@ -6,10 +6,10 @@ class profile::ssh_app{
     ensure => 'running',
     enable => 'true',
   }
-  ssh_authorized_key{'root@master.puppet.vm':
-    ensure=>absent,
+  ssh_authorized_key{'root@master.nis-puppet.vm':
+    ensure=>present,
     user=>'root',
     type=>'ssh-rsa',
-    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC7xE97MsVq/nNzLgrcM/AQr+0zS5SqTnNDJ/+mhdWaZk0ssZGYkIPKHLTyXmWgoi9JMRs5IixTJ0iZ8tEKuSg/IqpXIwQBTXP6VaH2doVYqKk4NA57nZmi+A1Qx3ux8wz5CBmwfrkbgxboBY5YW5D5GrwH9Bm81qoy+SJFPg5krgZhQb5xXFSg4GHYmRtw86OiofL/8zlLuDs0fSt5UOn5zMau/d6stYqa5Mgz5ufG7xGBoczloQtLleHpvsn4gQmEeR7DWlI+SHZpr/SPECaZ6sNJCsqfbtZMloGaTc3C677qGZy+UDGvnVeSUrGCvc66jU2nz1GISBJIrZvSe2DR',
+    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC/Mc3Pw9htxgPyG0l5+to8MSEgGq15KmCZJDe/tBcpsicfmhEEJcy/73HRtRqYgtAty3ETfF1UDvKb8Yyv8M2y0m6mY/6K3AF7QKs4nrBf+ya4/GgTNymenvSW7Kz6+714SLQCleEH3zWho6sT0NjJJ7ReCIh2T01U2sBt134kxM9zrSb9VZCsmpjM9m7GukmNg6BlLYza4JGwxvDaA/Eisw9Nc+nYFOue/4cGmg1LYBya2IqwZStbusUAbw7wy63UB6rp4vk4+Ls40wVMuB4Tl21qWLJbDvZ+aZgrIg4TdjOp7k7X9Jy9sQ4ZhReStIDOyuKgG5r5G/QhK5MTBIt9',
   }
 }
