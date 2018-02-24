07565b9d1661bd0172c832bfebf874f0e2bacb51
julio-hernandez <tenek75@gmail.com>
2018-02-23 22:04:12 -0500

    Update ubuntu_agent_nodes.pp

--- site/profile/manifests/ubuntu_agent_nodes.pp
+++ site/profile/manifests/ubuntu_agent_nodes.pp
@@ -3,4 +3,8 @@ class profile::ubuntu_agent_nodes{
     ensure =>present,
     ip =>'192.168.75.155',
   }
+  host{'client-1.example.com':
+    ensure => present,
+    ip     =>'192.168.75.196',
+  }
 }
